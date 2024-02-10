bd1e3dbdc183f23846007ff72039af5355bb12e4
Muhamad Jamil Firdaus <muhamadjamil668@gmail.com>
2024-02-10 08:34:16 +0700

    model addresses

--- app/Models/Contact.php
+++ app/Models/Contact.php
@@ -2,9 +2,9 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Contact extends Model
 {
@@ -16,6 +16,11 @@ class Contact extends Model
 
     public function user(): BelongsTo
     {
-        return $this->BelongsTo(Contact::class, 'user_id', 'id');
+        return $this->belongsTo(Contact::class, 'user_id', 'id');
+    }
+
+    public function address(): HasMany
+    {
+        return $this->hasMany(Address::class, 'contact_id', 'id');
     }
 }
